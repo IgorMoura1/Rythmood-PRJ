061f46fe24bfdcab3be7b069ffc4b2b55029cbe8
igormoura1 <igor.moura@sptech.school>
2023-11-30 17:23:35 -0300

    Organizando projeto

--- Site API/site/src/models/dashboardModel.js
+++ Site API/site/src/models/dashboardModel.js
@@ -7,6 +7,24 @@ function buscarDashboard() {
     return database.executar(instrucao);
 }
 
+
+
+function buscarRanking() {
+    var instrucao = `select score as pontuacao, count(score) as qtdPessoasQueAcertaram from ranking group by score order by score limit 5;`
+
+    console.log("Executando a instrução SQL \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function buscarFormulario() {
+    var instrucao = `select score as pontuacao, count(score) as qtdPessoasQueAcertaram from ranking group by score order by score limit 5;`
+
+    console.log("Executando a instrução SQL \n" + instrucao);
+    return database.executar(instrucao);
+}
+
 module.exports = {
-    buscarDashboard
+    buscarDashboard,
+    buscarRanking,
+    buscarFormulario
 }
